0c33afd9db49c605aa8f10627a1685a1451b2c73
yukito <iyggf66974@yahoo.ne.jp>
2025-02-10 17:48:33 +0900

    err

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { CreatePage, HomeLayout, Landing } from './pages'
+import SearchPage from './pages/SearchPage'
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomeLayout />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+      },
+      {
+        path: '/posts',
+        element: <SearchPage />,
+      },
+      {
+        path: '/create-post',
+        element: <CreatePage />,
+      },
+      
+    ]
+  }
+])
 const App = () => {
   return (
-    <div>App</div>
+    <RouterProvider router={router} />
   )
 }
 
---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+export { default as HomeLayout } from './HomeLayout';
+export { default as Landing } from './Landing';
+export { default as CreatePage } from './CreatePage';
+export { default as SearchPage } from './SearchPage';
