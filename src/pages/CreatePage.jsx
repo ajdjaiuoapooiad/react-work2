faf03f96b7d81c39bea59e14f36cd07e83064c0e
yukito <iyggf66974@yahoo.ne.jp>
2025-02-12 11:52:02 +0900

    err create

--- src/pages/CreatePage.jsx
+++ src/pages/CreatePage.jsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
+
+
 
 const CreatePage = () => {
+
+  const [ title,setTitle ] = useState('')
+  const [ income,setInocme ] = useState('')
+  const [ category,setCategory ] = useState('')
+  console.log(title, income, category);
+  
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    console.log(title, income, category);
+
+
+    try {
+      await axios.post('http://localhost:3001/api/v1/posts', {
+        title: title,
+        category: category,
+        income: income,
+      })
+      console.log('投稿しました');
+      
+    }catch (error) {
+      alert('投稿できませんでした')
+    }
+  }
+
   return (
-    <div>CreatePage</div>
+    <>
+      <div>CreatePage</div>
+      <form action="" onSubmit={handleSubmit}>
+        <input type="text" className='mx-5 my-12' onChange={(e) => setTitle(e.target.value)} /><br />
+        <input type="text" className='mx-5 my-12' onChange={(e) => setCategory(e.target.value)} /><br />
+        <input type="text" className='mx-5 my-12' onChange={(e) => setInocme(e.target.value)} /><br />
+        <button type='submit' className='btn bg-gray-500 rounded-md mx-12'>投稿する</button>
+      </form>
+    </>
   )
 }
 
