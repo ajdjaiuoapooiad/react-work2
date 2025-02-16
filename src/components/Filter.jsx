198d4f3f6b6a69a15a7e7ef1fa4fd3d9b6d0f06a
yukito <iyggf66974@yahoo.ne.jp>
2025-02-16 17:12:18 +0900

    err

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -9,6 +9,9 @@ import { Form } from 'react-router-dom'
 const Filter = () => {
   const list1 = categories
   const list2 = incomes
+  const search = ''
+  const category = ''
+  const income = ''
   
 
 
@@ -18,7 +21,18 @@ const Filter = () => {
       <div className='text-2xl font-bold p-3'>検索フォーム</div>
 
       
-      
+      <Form className='my-5 mx-12'>
+
+        <FormInput name='search' defaultValue={search}  />
+ 
+        <div className=''>カテゴリ</div>
+        <FormSelect name='category' list={list1} defaultValue={category} />
+
+        <div className='pt-10'>年収（万円）</div>
+        <FormSelect name='income'  list={list2} defaultValue={income} />
+
+        <button type='submit' className='btn bg-gray-800 rounded-md my-10 p-3 text-white hover:bg-gray-700' >検索</button>
+      </Form>
     </div>
   )
 }
