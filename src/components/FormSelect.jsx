ac1bced23c5e422586e05c8c3b94ec72fa2a41a0
yukito <iyggf66974@yahoo.ne.jp>
2025-02-17 10:44:52 +0900

    rr

--- src/components/FormSelect.jsx
+++ src/components/FormSelect.jsx
@@ -6,19 +6,19 @@ import db from '../../db';
 const FormSelect = ({name, list, defaultValue}) => {
   console.log(list);
 
-  const handleFilter = (e) => {
-    console.log(e.target.value);
-    // ��り込み処理
-    if (name === 'category') {
-      const filteredData = db.filter((item) => item.category === e.target.value)
-      console.log(filteredData);
-    } else {
-      const filteredData = db.filter((item) => item.income === Number(e.target.value))
-      console.log(filteredData);
-    }
+  // const handleFilter = (e) => {
+  //   console.log(e.target.value);
+  //   // ��り込み処理
+  //   if (name === 'category') {
+  //     const filteredData = db.filter((item) => item.category === e.target.value)
+  //     console.log(filteredData);
+  //   } else {
+  //     const filteredData = db.filter((item) => item.income === Number(e.target.value))
+  //     console.log(filteredData);
+  //   }
 
     
-  }
+  // }
     
   
   return (
@@ -28,7 +28,6 @@ const FormSelect = ({name, list, defaultValue}) => {
         className='rounded-2xl px-4 py-2'
         name={name}
         defaultValue={defaultValue}
-        onChange={(e) => handleFilter(e)}
         >
           {list.map((item) => {
             return (
