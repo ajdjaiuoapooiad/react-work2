da8923d479f27be6fb19b0dd5f9a0c1a5e5f54a6
yukito <iyggf66974@yahoo.ne.jp>
2025-02-17 11:33:32 +0900

    err

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,14 +7,14 @@ const Navbar = () => {
         <nav className='flex justify-between mx-auto container items-center p-3 text-white'>
                 
             <div className='text-3xl '>
-                <a href="/" className='hover:text-blue-500 transition-all duration-300'>求人検索アプリ</a>
+                <Link to={"/"} className='hover:text-blue-500 transition-all duration-300'>求人検索アプリ</Link>
             </div>
 
 
             <div className='space-x-12 font-bold'>
-                <a href="/" className='hover:text-blue-500 transition-all duration-300'>ホーム</a>
+                <Link to={"/"} className='hover:text-blue-500 transition-all duration-300'>ホーム</Link>
                 <Link to={"/create-post"} className='hover:text-blue-500 transition-all duration-300'>Create</Link>
-                <a href="/" className='hover:text-blue-500 transition-all duration-300'>問い合わせ</a>
+                <Link to={"/"} className='hover:text-blue-500 transition-all duration-300'>問い合わせ</Link>
             </div>
 
 
